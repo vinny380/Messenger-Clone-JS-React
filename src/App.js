88978e730b8709987fc5a9c1ba27494850acbeb2
Vinícius <vinny.purgato@gmail.com>
2020-07-11 14:09:12 -0300

    setting-environment

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+
+  
+  return (
+    <div className="App">
+      <h1>Vinny's Messenger</h1>
+
+      <input/>
+      <button>Send Message</button>
+      {/*input field*/}
+      {/*button*/}
+      
+      {/*messages*/}
+    </div>
+  );
+}
+
+export default App;
